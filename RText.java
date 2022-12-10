cd764e0d21ff7cc6cdc91007f6a039a4a5ffebe0
Leonid Matata <matatallika@gmail.com>
2022-12-11 00:40:26 +0200

    add first file

---
+++ RText.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class RText {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("AAAAAAAAA");
+        a.add("BBBBBBBBB");
+        a.add("CCCCCCCCCC");
+        a.add("DDDDDDDDDD");
+        a.add("EEEEEEEEEEE");
+        a.add("FFFFFFFFFFF");
+        a.add("GGGGGGGGGGGG");
+        a.add("IIIIIIIIIIIII");
+        a.add("JJJJJJJJJJJJJ");
+        for (String s : a) {
+            System.out.println(s);
+        }
+
+    }
+}
